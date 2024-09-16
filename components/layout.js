d279cf3c174b5357bcdb916d8dfb180732aa4d51
mateo aita <mateoaita@gmail.com>
2024-09-16 16:23:00 -0300

    analitica de vercel

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,22 @@
-import React from 'react';
-  import styles from '../styles/layout.module.css'
+// import React from 'react';
+//   import styles from '../styles/layout.module.css'
 
-  export default function Layout(props) {
-    return (
-      <div className={styles.layout}>
-        {props.children}
-      </div>
-    )
-  }
+//   export default function Layout(props) {
+//     return (
+//       <div className={styles.layout}>
+//         {props.children}
+//       </div>
+//     )
+//   }
+import React from 'react';
+import styles from '../styles/layout.module.css';
+import { Analytics } from "@vercel/analytics/react";
+
+export default function Layout(props) {
+  return (
+    <div className={styles.layout}>
+      {props.children}
+      <Analytics /> {/* Añadir Analytics aquí */}
+    </div>
+  );
+}
